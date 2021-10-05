d55bb8d6cdd0f00bd945237f5f2303cdaec74ff4
Ana Carolina K <anacko@gmail.com>
2021-10-05 15:51:32 -0400

    paint it REDgit add .!

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -4,11 +4,20 @@
 
 $(() => {
   $( "#tweet-text" )
-    .keydown(function() {
+    .on("input", function() {
+
+      // Count the amount of chars typed and write the difference in the counter field
       let charsTyped = $( this ).val();
-      const maxLength = 140; // $( this ).attr( 'maxlength' );
-      const newTweetCounter = $(this).parent().find('.counter');
+      const maxLength = 140; // hardcoded. If there was a maxlength for textarea: $( this ).attr( 'maxlength' );
+      const newTweetCounter = $( this ).parent().find('.counter');
       $(newTweetCounter).text(maxLength - charsTyped.length);
+
+      // If the user overtped, the number will turn red.
+      if (charsTyped.length > maxLength) {
+        $(newTweetCounter).addClass("red");
+      } else {
+        $(newTweetCounter).removeClass("red");
+      }
     })
 
 });
