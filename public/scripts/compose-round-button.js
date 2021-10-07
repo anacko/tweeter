860025b0beb185869213d89c86edcfe058e9d7bb
Ana Carolina K <anacko@gmail.com>
2021-10-07 10:07:47 -0400

    add big red round button at the bottom to scroll up

--- public/scripts/compose-round-button.js
+++ public/scripts/compose-round-button.js
@@ -2,8 +2,17 @@
 
 $(() => {
 
-  $(window).on('scroll', function() {
+  $('#big-round-red-button').hide();
 
+  $(window).on('scroll', function() {
+    const $redButton = $('#big-round-red-button')
+    const $scrollFromTop = $(this).scrollTop();
+  
+    if ($scrollFromTop > 300) {
+      $redButton.fadeIn(200);
+    } else {
+      $redButton.fadeOut(200);
+    }
   })
 
 
